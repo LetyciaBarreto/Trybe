7aad50eb992ccf90cad81dabe3119f4ab234e804
Letycia Barreto <letyciasbarreto@gmail.com>
2021-02-24 20:56:43 -0300

    exercicio 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises/exe-1_dia-4.1.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = 15;
+console.log("Soma = "+ (a + b));
+console.log("Subtração = "+ (a - b));
+console.log("Multiplicação = "+ (a * b));
+console.log("Divisão = "+ (a / b));
+console.log("Módulo= "+ (a % b));
