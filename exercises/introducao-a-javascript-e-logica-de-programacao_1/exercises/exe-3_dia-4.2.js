f1fb8b00fa0a44928d24a995285ab1629ff02a47
Letycia Barreto <letyciasbarreto@gmail.com>
2021-02-24 21:18:03 -0300

    exercicio 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises/exe-3_dia-4.2.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b =60;
+var c= 30;
+ if ((a > b) && (a > c)){
+     console.log("O número " +a +" é o maior ");
+ } else if ((b > a) && (b > c)) {
+     console.log("O número "+b +" é o maior");
+ } else{
+     console.log("O número "+c +" é o maior");
+ }
